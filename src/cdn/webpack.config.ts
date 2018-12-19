a2a0c1eab99bfb3b3997d5b47dbe9f9a0c324bcc
David Festal <dfestal@redhat.com>
2018-12-19 22:14:23 +0100

    tests of `webpack.config.ts`
    Signed-off-by: David Festal <dfestal@redhat.com>

--- src/cdn/webpack.config.ts
+++ src/cdn/webpack.config.ts
@@ -8,11 +8,7 @@
 * SPDX-License-Identifier: EPL-2.0
 **********************************************************************/
 
-import * as decls from './base';
-const path = require('path');
-const webpack = require('webpack');
-const fs = require('fs');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import { customizeWebpackConfig } from './webpack-customizer';
 
 const yargs = require('yargs');
 const { env: { cdn, monacopkg, monacohtmlcontribpkg, monacocsscontribpkg } } = yargs.option('env.cdn', {
@@ -38,113 +34,10 @@ const { env: { cdn, monacopkg, monacohtmlcontribpkg, monacocsscontribpkg } } = y
     default: ''
 }).argv;
 
-let theiaCDN = '';
-let monacoCDN = '';
-
-if (cdn) {
-    if (fs.existsSync(cdn)) {
-        const cdnJson = JSON.parse(fs.readFileSync(cdn, 'utf8'));
-        theiaCDN = cdnJson.theia;
-        monacoCDN = cdnJson.monaco;
-    }
-}
-
-if (monacoCDN &&
-    !(monacopkg && monacohtmlcontribpkg && monacocsscontribpkg)) {
-    throw new Error("Please check that you specified the three parameters: '--env.monacopkg', '--env.monacohtmlcontribpkg', '--env.monacocsscontribpkg'");
-}
-
 // Retrieve the default, generated, Theia Webpack configuration
 const baseConfig = require('../webpack.config');
 
-if (theiaCDN || monacoCDN) {
-
-    // Add the cdn-support.js file at the beginning of the entries.
-    // It contains the logic to load various types of files from the configured CDN
-    // if available, or fallback to the local file
-    const originalEntry = baseConfig.entry;
-    baseConfig.entry = {
-        'cdn-support': path.resolve(__dirname, 'bootstrap.js'),
-        'theia': originalEntry
-    };
-
-    // Include the content hash to enable long-term caching
-    baseConfig.output.filename = '[name].[chunkhash].js';
-
-    const extensions = fs.existsSync('extensions.json') ? JSON.parse(fs.readFileSync('extensions.json', 'utf8'))
-        .extensions.map((ext: decls.Extension) => ext.name) : [];
-
-    // Separate the webpack runtime module, theia modules, external vendor modules
-    // in 3 distinct chhunks to optimize caching management
-    baseConfig.optimization = {
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                che: {
-                    test(module: any, chunks: any) {
-                        const req = module.userRequest;
-                        return req && (req.endsWith('/src-gen/frontend/index.js') ||
-                            extensions.some((name: string) => req.includes('/' + name + '/')));
-                    },
-                    name: 'che',
-                    chunks: 'all',
-                    enforce: true,
-                    priority: 1
-                },
-                vendors: {
-                    test: /[\/]node_modules[\/](?!@theia[\/])/,
-                    name: 'vendors',
-                    chunks: 'all',
-                    enforce: true
-                }
-            }
-        }
-    };
-
-    // Use our own HTML template to trigger the CDN-supporting
-    // logic, with the CDN prefixes passed as env parameters
-    baseConfig.plugins.push(new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'cdn/custom-html.html',
-        inject: false,
-        customparams: {
-            cdnPrefix: theiaCDN,
-            monacoCdnPrefix: monacoCDN,
-            cachedChunkRegexp: '^(theia|che|vendors)\.[^.]+\.js$',
-            cachedResourceRegexp: '^.*\.(wasm|woff2|gif)$',
-            monacoEditorCorePackage: monacopkg,
-            monacoHtmlContribPackage: monacohtmlcontribpkg,
-            monacoCssContribPackage: monacocsscontribpkg
-        }
-    }));
-
-    // Use hashed module IDs to ease caching support
-    // and avoid the hash-based chunk names being changed
-    // unexpectedly
-    baseConfig.plugins.push(new webpack.HashedModuleIdsPlugin());
-
-    // Insert a custom loader to override file and url loaders,
-    // in order to insert CDN-related logic
-    baseConfig.module.rules.filter((rule: any) => rule.loader && rule.loader.match(/(file-loader|url-loader)/))
-        .forEach((rule: any) => {
-            const originalLoader: any = {
-                loader: rule.loader
-            };
-
-            if (rule.options) {
-                originalLoader.options = rule.options;
-            }
-
-            delete rule.options;
-            delete rule.loader;
-            rule.use = [
-                {
-                    loader: path.resolve('cdn/webpack-loader.js'),
-                },
-                originalLoader
-            ];
-        });
-}
+customizeWebpackConfig(cdn, monacopkg, monacohtmlcontribpkg, monacocsscontribpkg, baseConfig);
 
 // Export the customized webpack configuration object
 module.exports = baseConfig;
